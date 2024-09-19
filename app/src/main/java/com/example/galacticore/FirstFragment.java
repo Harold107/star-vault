9af694f48daf276075832627809ec71a45edebbf
Theodorous Soliman <TMSoliman@student.fullsail.edu>
2024-09-19 16:43:13 -0400

    added basic first fragment to display and test transaction page

---
+++ app/src/main/java/com/example/galacticore/FirstFragment.java
@@ -0,0 +1,31 @@
+package com.example.galacticore;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.example.galacticore.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // You can add any initialization code here
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
