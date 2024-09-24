3b7dafe10d707fc8edaa6ef35f7454a0157e970c
Harold107 <haroldaro0216@gmail.com>
2024-09-24 18:46:55 -0400

    Test commit

--- app/src/main/java/com/example/galacticore/HomeFragment.java
+++ app/src/main/java/com/example/galacticore/HomeFragment.java
@@ -45,7 +45,7 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-
+        //asdfasdfasf
 
         return view;
     }
